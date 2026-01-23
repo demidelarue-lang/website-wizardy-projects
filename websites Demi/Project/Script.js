27f61ead9f9edaddc3135650841426e5e1d8f802
demidelarue-lang <demidelarue@gmail.com>
2026-01-23 17:59:57 +0400

    Add files via upload

---
+++ websites Demi/Project/Script.js
@@ -0,0 +1,40 @@
+var summer ="yellow";
+var autumn ="orange";
+var winter ="lightblue";
+var spring ="lightgreen";
+
+function changeImg1(){
+document.getElementById("imgMain").search("https://images.squarespace-cdn.com/content/v1/5811566d20099e23814644fd/1580754598184-ATYRJ25IPF6ZSV87J851/early-spring-care-for-flowering-trees-shrubs-ornamental-cherry.jpg");
+document.getElementById("season").innerHTML="Spring";
+document.getElementById("season").style.color = spring; 
+}
+
+function changeImg2(){
+document.getElementById("imgMain").search("https://img.traveltriangle.com/blog/wp-content/uploads/2019/06/cover-for-Autumn-In-Korea.jpg");
+document.getElementById("season").innerHTML="Autumn";
+document.getElementById("season").style.color = autumn; 
+}
+
+function changeImg3(){
+document.getElementById("imgMain").search("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beautiful-tropical-sunset-scenery-two-sun-beds-royalty-free-image-1595368231.jpg");
+document.getElementById("season").innerHTML="Summer";
+document.getElementById("season").style.color = summer; 
+}
+
+function changeImg4(){
+document.getElementById("imgMain").search("https://i.natgeofe.com/k/20b88422-ae27-4b5d-af43-9eb76a23a477/WH-Solstice_4x3.jpg");
+document.getElementById("season").innerHTML="Winter";
+document.getElementById("season").style.color = winter; 
+}
+
+function changeImg5(){
+document.getElementById("imgMain").search("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/autumn-in-the-white-mountains-of-new-hampshire-royalty-free-image-841380450-1531931081.jpg?crop=1.00xw:0.755xh;0,0.202xh&resize=1200:*");
+document.getElementById("season").innerHTML="Autumn";
+document.getElementById("season").style.color = autumn; 
+}
+
+function changeImg6(){
+document.getElementById("imgMain").search("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg");
+document.getElementById("season").innerHTML="Winter";
+document.getElementById("season").style.color = winter; 
+}
